2e942b334ad27fb0fd64d4f5ec0abbd8a847289b
alriansyah <alriansyah1218@gmail.com>
2023-10-16 02:04:07 +0700

    Type alias

---
+++ tests/6.type-alias.test.ts
@@ -0,0 +1,22 @@
+import { Category, Product } from '../src/2.type-alias';
+
+describe("Type Alias", () => {
+    it("should support in type", () => {
+
+        const category: Category = {
+            id: 1,
+            name: "Eko"
+        }
+
+        const product: Product = {
+            id: "1",
+            name: "Samsung Galaxy S23 Ultra",
+            price: 23000000,
+            category: category
+        }
+
+        console.log(product);
+        console.log(category); 
+
+    });
+});
