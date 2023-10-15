0fb6ebd26d1eedc546489ec2c408e6b1539150bc
alriansyah <alriansyah1218@gmail.com>
2023-10-15 20:07:02 +0700

    Babel Typescript

--- tests/2.tipe-data.test.ts
+++ tests/2.tipe-data.test.ts
@@ -1,14 +1,17 @@
 describe("Data Type", () => {
     it("should must declarate", () => {
         
-        const name: string = "Eko Kurniawan Khannedy";
-        const balance: number = 100000;
-        const isVip: boolean = true;
+        let name: string = "Eko Kurniawan Khannedy";
+        let balance: number = 100000;
+        let isVip: boolean = true;
 
         console.log(name);
         console.log(balance);
         console.log(isVip);
         
+        // name = 4000
+        // balance = "100000"
+        // isVip = 4
 
     })
 })
