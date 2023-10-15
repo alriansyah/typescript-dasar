d155d92df565c8b4120cc1b58360b9b82f646e58
alriansyah <alriansyah1218@gmail.com>
2023-10-16 03:16:15 +0700

    Function interface

--- tests/10.interface.test.ts
+++ tests/10.interface.test.ts
@@ -10,10 +10,24 @@ describe("Interface", () => {
             npwp: "456"
         }
 
-        
+
         console.log(seller);
         seller.name = "budi";
         console.log(seller);
 
     });
+
+    it("should supportfunction interface", () => {
+
+        interface AddFunction {
+            (value1: number, value2: number): number; // (parameter1, parameter2) => tipe return value
+        }
+
+        const add: AddFunction = (value1: number, value2: number): number => {
+            return value1 + value2;
+        }
+
+        expect(add(1, 2)).toBe(3);
+
+    })
 })
