c4529ef7a0aa94d1299229fb9fb6896ec84ad3a1
alriansyah <alriansyah1218@gmail.com>
2023-10-16 04:42:37 +0700

    Function Parameter

--- tests/11.function.test.ts
+++ tests/11.function.test.ts
@@ -1,7 +1,7 @@
 import { sayHello } from '../src/1.say-hello';
 describe("Function", () => {
     it("should support in typescript", () => {
-        
+
         function sayHello(name: string): string {
             return `Hello ${name}`;
         }
@@ -15,4 +15,42 @@ describe("Function", () => {
         printHello("Eko");
 
     })
+
+    it("should support default value", () => {
+
+        function sayHello(name: string = "Guest"): string {
+            return `Hello ${name}`;
+        }
+
+        expect(sayHello()).toBe("Hello Guest");
+        expect(sayHello("Eko")).toBe("Hello Eko");
+    });
+
+    it("should support rest parameter", () => {
+
+        function sum(...values: number[]): number {
+            let total = 0;
+            for (const value of values) {
+                total += value;
+            }
+            return total;
+        }
+
+        expect(sum(1, 2, 3, 4, 5)).toBe(15);
+    })
+
+    it("should support optional parameter", () => {
+
+        function sayHello(firstName: string, lastName?: string): string {
+            if (lastName) {
+                return `Hello ${firstName} ${lastName}`;
+            } else {
+                return `Hello ${firstName}`;
+            }
+        }
+
+        expect(sayHello("Eko")).toBe("Hello Eko");
+        expect(sayHello("Eko", "Kurniawan")).toBe("Hello Eko Kurniawan");
+
+    });
 })
