28dd5627a26a757b649d498f48f4c95884b643cc
alriansyah <alriansyah1218@gmail.com>
2023-10-15 19:56:45 +0700

    Tipe data primitif

---
+++ tests/2.tipe-data.test.ts
@@ -0,0 +1,14 @@
+describe("Data Type", () => {
+    it("should must declarate", () => {
+        
+        const name: string = "Eko Kurniawan Khannedy";
+        const balance: number = 100000;
+        const isVip: boolean = true;
+
+        console.log(name);
+        console.log(balance);
+        console.log(isVip);
+        
+
+    })
+})
